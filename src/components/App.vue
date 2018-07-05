b34bcd2f4c0d9efcac61b629d35c047344b0121b
VaranRohila <varansinghrohila@gmail.com>
2018-07-06 00:43:23 +0530

    action added to store

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,19 @@
 <template >
   <div id="app">
-    Got jokes?
-
+    <h3>Got jokes?</h3>
+    <button type="button" class="btn btn-primary" name="button" @click="initJokes">Add 10 Random Jokes</button>
+    <div class="container-fluid">
     {{$store.state}}
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 
+export default {
+    methods: mapActions([
+      'initJokes'
+    ])
+  }
 </script>
